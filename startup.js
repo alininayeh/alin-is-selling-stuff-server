c3e5ea2782f0e65274ffe4a45072d1fd381258b4
Alin Inayeh <>
2019-06-26 20:58:31 +0200

    ...

--- startup.js
+++ startup.js
@@ -1,7 +1,7 @@
 const Database = require('./src/database');
 
 async function initDatabase() {
-    console.log('Creating a new database if not present');
+    console.log('Creating a new database if not present - v1');
     await Database.connect();
 
     console.log('Creating the products collection');
